fb7df848825f10e6d0fcdaba4478020a946ffbac
Denys Lahoda <denys.lahoda@gmail.com>
2019-03-07 11:58:31 +0200

    Setup startRemoveExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -5,6 +5,7 @@ import {
   addExpense,
   editExpense,
   removeExpense,
+  startRemoveExpense,
   setExpenses,
   startSetExpenses
 } from "../../actions/expenses";
@@ -31,6 +32,23 @@ test("should setup remove expense action object", () => {
   expect(action).toEqual({
     type: "REMOVE_EXPENSE",
     id: "123abc"
+  });
+});
+
+test("should remove expense from firebase", (done) => {
+  const store = createMockStore({});
+  const id = expenses[2].id;
+
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: "REMOVE_EXPENSE",
+      id
+    });
+    return database.ref(`expenses/${id}`).once("value");
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy();
+    done();
   });
 });
 
@@ -134,3 +152,5 @@ test("should fetch the expenses from firebase", (done) => {
     done();
   });
 });
+
+
