69cc8aedfa5cb16dfd17a229204fa0f0a40afdcf
Denys Lahoda <denys.lahoda@gmail.com>
2019-03-08 10:12:05 +0200

    Add private routes

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ import { Provider } from 'react-redux';
 import "normalize.css";
 import "./styles/style.scss";
 import "react-dates/lib/css/_datepicker.css";
-import AppRouter from "./routers/AppRouter";
+import AppRouter, { history } from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import "./firebase/firebase";
-import { startSetExpenses } from "./actions/expenses"
+import { firebase } from "./firebase/firebase";
+import { startSetExpenses } from "./actions/expenses";
+import { login, logout } from "./actions/auth";
 
 const store = configureStore();
 const jsx = (
@@ -16,9 +17,32 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById("app"));
+    hasRendered = true;
+  }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 
-store.dispatch(startSetExpenses()).then(() => {
-  
-  ReactDOM.render(jsx, document.getElementById("app"));
+
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // Log in
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === "/") {
+        history.push("/dashboard");
+      }
+    });
+  } else {
+    // Log out
+    store.dispatch(logout());
+    renderApp();
+    history.push("/");
+  }
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from "react-redux";
+import { startLogout } from "../actions/auth";
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <button onClick={startLogout}>Logout</button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispath) => ({
+  startLogout: () => dispath(startLogout())
+});
+
+export default connect(null, mapDispatchToProps)(Header);
