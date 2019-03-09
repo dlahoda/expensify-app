876261d9b009339a44c76284c231ad6a110203e4
Denys Lahoda <denys.lahoda@gmail.com>
2019-03-09 10:40:35 +0200

    Added ExpensesSummary styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <button onClick={startLogout}>Logout</button>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Expensify</h1>
+        </Link>
+        <button className="button button--transparent" onClick={startLogout}>
+          Logout
+        </button>
+      </div>
+    </div>
   </header>
 );
 
-const mapDispatchToProps = (dispath) => ({
+const mapDispatchToProps = dispath => ({
   startLogout: () => dispath(startLogout())
 });
 
-export default connect(null, mapDispatchToProps)(Header);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Header);
+
+// <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
